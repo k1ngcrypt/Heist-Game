69faa77cd5f84a1455edafb26b32460a355950b0
k1ngcrypt <118375520+k1ngcrypt@users.noreply.github.com>
2026-05-06 16:25:01 -0400

    Sprite Implementation

--- Assets/Scripts/Door/Modules/Openable.cs
+++ Assets/Scripts/Door/Modules/Openable.cs
@@ -2,13 +2,35 @@ using UnityEngine;
 
 namespace HeistGame.Door
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class Openable : MonoBehaviour, IDoorOpenBehavior
     {
         [SerializeField] private bool isOpen;
         [SerializeField] private Sprite openSprite;
         [SerializeField] private Sprite closeSprite;
 
+        private SpriteRenderer spriteRenderer;
+
         public bool IsOpen => isOpen;
+
+        private void OnValidate()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            if (openSprite != null)
+            {
+                spriteRenderer.sprite = openSprite;
+            }
+            else if (closeSprite != null)
+            {
+                spriteRenderer.sprite = closeSprite;
+            }
+        }
+
+        private void OnEnable()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+         
 
         public void OpenDoor()
         {
@@ -18,6 +40,7 @@ namespace HeistGame.Door
             }
 
             isOpen = true;
+            spriteRenderer.sprite = openSprite;
         }
 
         public void CloseDoor()
@@ -28,6 +51,7 @@ namespace HeistGame.Door
             }
 
             isOpen = false;
+            spriteRenderer.sprite = closeSprite;
         }
     }
 }
