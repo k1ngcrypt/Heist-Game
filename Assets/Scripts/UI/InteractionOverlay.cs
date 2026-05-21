b719c4c341aefee42941af871cf3e857035be9b8
GrnTortoise <212028919+GrnTortoise@users.noreply.github.com>
2026-05-20 21:14:05 -0400

    Edited UI to make it work. Need to make 2d collider work (IDK WHY AAAHAHAAHAHHA) and make prompt before showing menu

--- Assets/Scripts/UI/InteractionOverlay.cs
+++ Assets/Scripts/UI/InteractionOverlay.cs
@@ -1,26 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class InteractionOverlay : MonoBehaviour
 {
-    string txt;
-    [SerializeField] private GameObject interactionOverlay;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private Transform panel;
     [SerializeField] private TextMeshProUGUI titleText;
-    [SerializeField] private Button interactButton;
+    [SerializeField] private Button btnPrefab;
 
-    public delegate void InteractAction();
-
-    public void Start()
-    {
-        interactionOverlay.SetActive(false);
-    }
-
-    public void Initialize(string title, InteractAction action)
+    public void Initialize(string title, List<InteractBtnTemplate> actions, Vector3 worldPosition)
     {
         titleText.text = title;
-        interactButton.onClick.RemoveAllListeners();
-        interactButton.onClick.AddListener(() => action());
-        interactionOverlay.SetActive(true);
+        canvas.GetComponent<Transform>().position = worldPosition - new Vector3(0, 1.25f, 0); // One and a quartertile down
+
+        List<Button> buttons = new List<Button>();
+        foreach (var btnData in actions)
+        {
+            Button btn = Instantiate(btnPrefab, panel);
+            btn.GetComponentInChildren<TMP_Text>().text = btnData.text;
+
+            btn.onClick.AddListener(() =>
+            {
+                btnData.onClick?.Invoke();
+            });
+            buttons.Add(btn);
+        }
+
+        //resize panel to fit buttons
+        var layout = panel.GetComponent<VerticalLayoutGroup>();
+        float paddingBottom = layout.padding.bottom;
+
+        var pnl = panel.GetComponent<RectTransform>();
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(pnl);
+        var lastBtn = buttons[^1];
+        float pos = lastBtn.GetComponent<RectTransform>().anchoredPosition.y;
+        float height = lastBtn.GetComponent<RectTransform>().rect.height;
+
+
+        pnl.sizeDelta = new Vector2(pnl.sizeDelta.x, -(pos - (height*0.5f) - paddingBottom));
     }
 }
