799c3b3debdf04e5b8cfa033601f65df33a1d627
FloorNickel <niok.coding@gmail.com>
2026-06-04 10:38:20 -0400

    Optimization and Light Baking but shadows are broken

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -27,6 +27,13 @@ public class CameraManager : MonoBehaviour, ITurnActor
     [SerializeField] [Range(0f,360f)] private float sunDirection = 0f;
     [SerializeField] [Range(0f,1f)] private float sunLowering = 0.6f;
     [SerializeField] [Range(0f,100f)] private float sunExtraDistanceFromTilemap = 14;
+    [Header("Light Baking")]
+    [SerializeField] private bool bakeLights = true;
+    [SerializeField] private bool alwaysBake = false;
+    [SerializeField] [Range(0, 50)] private int lightTextureResolution = 30;
+    [SerializeField] private GameObject lightFloored;
+    [SerializeField] private GameObject bakedLightLocations;
+    [SerializeField] private GameObject bakedLightLocationsLocationns;
     [Header("Backround Removal")]
     [SerializeField] private GameObject sinRadiance;
     [SerializeField] private GameObject playerLight;
@@ -46,6 +53,7 @@ public class CameraManager : MonoBehaviour, ITurnActor
     private Vector3 velocity = Vector3.zero;
     private Camera mainCamera;
     private Shader shader;
+    private readonly List<RenderTexture> textures = new();
     private const int SHADOWEXTRASIDESIZES = 2;
     private const float AC = Mathf.PI/180f;
     private readonly List<Camera> cameras = new(), coolCameras = new();
@@ -64,6 +72,8 @@ public class CameraManager : MonoBehaviour, ITurnActor
             Map.layerLocations = locations;
             Map.ReloadLayerBounds();
             layerBounds = Map.LayerBounds;
+            mainCamera = GetComponent<Camera>();
+            if (Application.isPlaying) return;
             
             //Setup Vents if needed
             if (isTopLocationVents||vents!=null) {
@@ -76,6 +86,97 @@ public class CameraManager : MonoBehaviour, ITurnActor
                 }
             }
 
+            //Bake Lights
+            if ((bakeLights||alwaysBake)&&lightFloored!=null&&lightTextureResolution>0&&bakedLightLocations!=null&&bakedLightLocationsLocationns!=null) {
+                bakeLights = false;
+
+                //Get Formats
+                List<GraphicsFormat> supportedFormats = new() {
+                    GraphicsFormat.R8G8B8_UNorm,
+                    GraphicsFormat.R8G8B8_SNorm,
+                    GraphicsFormat.R8G8B8_SInt,
+                    GraphicsFormat.R8G8B8_SRGB,
+                    GraphicsFormat.R8G8B8A8_UNorm,
+                    GraphicsFormat.R8G8B8A8_SNorm,
+                    GraphicsFormat.R8G8B8A8_SNorm,
+                    GraphicsFormat.R8G8B8A8_SInt,
+                    GraphicsFormat.R8G8B8A8_SRGB
+                }, depthFormats = new() {
+                    GraphicsFormat.D16_UNorm,
+                    GraphicsFormat.D16_UNorm_S8_UInt,
+                    GraphicsFormat.D24_UNorm,
+                    GraphicsFormat.D24_UNorm_S8_UInt,
+                    GraphicsFormat.D32_SFloat,
+                    GraphicsFormat.D32_SFloat_S8_UInt
+                };
+                GraphicsFormat format = supportedFormats[0], dFormat = depthFormats[0];
+                for (int i = 0; i < supportedFormats.Count && !SystemInfo.IsFormatSupported(format, GraphicsFormatUsage.Render); i++)
+                    format = supportedFormats[i];
+                if (!SystemInfo.IsFormatSupported(format, GraphicsFormatUsage.Render)) {
+                    Debug.LogError("[CameraManager] No suitable baking RenderTexture format found!");
+                    return;
+                }
+                for (int i = 0; i < depthFormats.Count && !SystemInfo.IsFormatSupported(dFormat, GraphicsFormatUsage.Render); i++)
+                    dFormat = depthFormats[i];
+                if (!SystemInfo.IsFormatSupported(dFormat, GraphicsFormatUsage.Render)) {
+                    Debug.LogError("[CameraManager] No suitable baking DepthStencilFormat found!");
+                    return;
+                }
+
+                //Setup GameObjects
+                for (int i = bakedLightLocationsLocationns.transform.childCount-1; i >= 0; i--) {
+                    Light2D l = bakedLightLocationsLocationns.transform.GetChild(i).GetComponent<Light2D>();
+                    if (l != null) {
+                        DestroyImmediate(l.lightCookieSprite);
+                        l.lightCookieSprite = null;
+                    }
+                    DestroyImmediate(bakedLightLocationsLocationns.transform.GetChild(i).gameObject);
+                }
+                if (playerLight) playerLight.SetActive(false);
+                if (mainCamera) mainCamera.enabled = false;
+                for (int i = 0; i<Map.layerLocations.Count; i++) {
+                    var bounds = Map.LayerBounds[i];
+
+                    //Create render Texture
+                    RenderTexture rt = new((bounds.size.x+SHADOWEXTRASIDESIZES)*lightTextureResolution,(bounds.size.y+SHADOWEXTRASIDESIZES)*lightTextureResolution, 0, format);
+                    rt.depthStencilFormat = dFormat;
+                    rt.Create();
+                    
+                    //Create Block
+                    GameObject block = Instantiate(lightFloored, new Vector3(bounds.center.x+0.5f, bounds.center.y+0.5f, 0), Quaternion.identity);
+                    block.transform.localScale = new Vector3(bounds.size.x+SHADOWEXTRASIDESIZES+0.21f, bounds.size.y+SHADOWEXTRASIDESIZES+0.21f, 1);
+
+                    //Create Camera
+                    GameObject coolCamera = Instantiate(shadowCam, Vector3.zero, Quaternion.identity);
+                    coolCamera.transform.position = new Vector3(bounds.center.x+0.5f, bounds.center.y+0.5f, -10);
+                    Camera cool = coolCamera.GetComponent<Camera>();
+                    cool.orthographicSize = (bounds.size.y+SHADOWEXTRASIDESIZES)*0.5f;
+                    cool.targetTexture = rt;
+                    cool.cullingMask = ~((1 << LayerMask.NameToLayer("ShadowLayer"))|(1 << LayerMask.NameToLayer("UI")));
+                    cool.Render(); //Fix This
+
+                    //Create Texture2D
+                    Texture2D tex = new(rt.width, rt.height, TextureFormat.RGBA32, false);
+                    RenderTexture.active = rt;
+                    tex.filterMode = FilterMode.Bilinear;
+                    tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+                    tex.Apply();
+                    RenderTexture.active = null;
+
+                    //Create Light
+                    GameObject light = Instantiate(bakedLightLocations, new Vector3(bounds.center.x+0.5f, bounds.center.y+0.5f, 0), Quaternion.identity);
+                    light.transform.parent = bakedLightLocationsLocationns.transform;
+                    light.GetComponent<Light2D>().lightCookieSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one*0.5f, lightTextureResolution);
+
+                    //Cleanup
+                    rt.Release();
+                    DestroyImmediate(coolCamera);
+                    DestroyImmediate(block);
+                }
+                if (playerLight) playerLight.SetActive(true);
+                if (mainCamera) mainCamera.enabled = true;
+            }
+            
             //The Sun
             if (hasSun&&sunAmount>0&&sunLight!=null&&sunLightLocations!=null) {
                 for (int i = sunLightLocations.transform.childCount-1; i >= 0; i--) 
@@ -226,7 +327,10 @@ public class CameraManager : MonoBehaviour, ITurnActor
 
         //Have Camera Blit Alpha of the Floor Onto shadow render texture, then set culling layers to ShadowLayer
         Material m = new(Shader.Find("Custom/allAlpha"));
-        if (sinRadiance) sinRadiance.SetActive(false);
+        if (sinRadiance) {
+            sinRadiance.SetActive(false);
+            Destroy(sinRadiance); //The reason for both of these is because Destroy isn't immediate, and DestroyImmediate isn't safe to use during runtime. Let it be.
+        }
         if (playerLight) playerLight.SetActive(false);
         if (BaseWall) BaseWall.SetActive(false);
         for (int i = 0; i < inputRT.Count; i++) {
@@ -251,7 +355,6 @@ public class CameraManager : MonoBehaviour, ITurnActor
             var cameraData = coolCameras[i].GetUniversalAdditionalCameraData();
             cameraData.renderPostProcessing = enabled;
         }
-        if (sinRadiance) sinRadiance.SetActive(true);
         if (playerLight) playerLight.SetActive(true);
         if (BaseWall) BaseWall.SetActive(true);
 
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 using HeistGame.Door;
 using HeistGame.Objectives;
 using UnityEditor.Experimental.GraphView;
@@ -11,6 +12,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float gridSize = 1f;
     [SerializeField] private LayerMask wallLayer;
     [SerializeField] private AwarenessManager awarenessManager;
+    [SerializeField] private Tilemap floorTilemap;
+    [SerializeField] private Tilemap baseTilemap;
     
     private bool isMoving = false;
     private bool inVent = false;
@@ -69,6 +72,7 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.position = endPosition;
+        CheckGround();
         awarenessManager.MakeSound(endPosition, 0.8f); // Make noise on move
         await TurnManager.Instance.ProcessTicks(inVent ? ventMoveTicks : 1);
         await Awaitable.WaitForSecondsAsync(interactionDuration);
@@ -138,5 +142,16 @@ public class PlayerController : MonoBehaviour
         }
         
         isMoving = false;
+    }
+
+    private void CheckGround() {
+        if (floorTilemap == null || baseTilemap == null || !Map.IsInitialized) return;
+        Vector3Int pos = floorTilemap.WorldToCell(transform.position);
+        if (!floorTilemap.HasTile(pos) && !baseTilemap.HasTile(pos)) {
+            int layer = Map.CurrentLayer();
+            if (layer==0) return;
+            Vector3Int lPos = pos + new Vector3Int((int)(Map.layerLocations[layer-1].x - Map.layerLocations[layer].x), (int)(Map.layerLocations[layer-1].y - Map.layerLocations[layer].y), 0);
+            if (Map.IsNull(lPos)) transform.position = lPos + new Vector3(0.5f, 0.5f, 0);
+        }
     }
 }
