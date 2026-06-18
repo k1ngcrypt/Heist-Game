32ebd8453f6d2cd4f8a6c3f0bc1e7eada05872db
FloorNickel <niok.coding@gmail.com>
2026-06-18 12:38:36 -0400

    FixesForBuildsDoneWOOOOOOOOOOOOOOOO

--- Assets/Scripts/UI/Interaction Overlay/InteractArea.cs
+++ Assets/Scripts/UI/Interaction Overlay/InteractArea.cs
@@ -4,7 +4,6 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.UI;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEditor.Overlays;
 
 [Serializable]
 public class InteractBtnTemplate
--- Assets/Scripts/UI/Loadout/LoadoutManager.cs
+++ Assets/Scripts/UI/Loadout/LoadoutManager.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 [System.Serializable]
 public class AreaList
